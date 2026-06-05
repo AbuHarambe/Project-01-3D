69972dc63a0584c6b60c742be775553389bf40e7
Desktop Haydar <haydar21.12@hotmail.com>
2026-06-05 18:44:05 +0200

    First Working animations

--- Characters/TestCharacter/test_character.gd
+++ Characters/TestCharacter/test_character.gd
@@ -2,11 +2,12 @@ extends CharacterBody3D
 
 @export var move_speed: float = 8.0
 @export var jump_force: float = 16.0
-
+enum States {IDLE, RUNNING, JUMPING, FALLING, ATTACK, DASH, LOCKED}
+var state: States = States.IDLE
 # Using Godot's project settings gravity default for 3D
 @export var gravity: float = 30.0
 
-func _physics_process(delta: float) -> void:
+func _process(delta: float) -> void:
 	handle_horizontal_movement()
 	apply_gravity(delta)
 	handle_jump()
@@ -18,6 +19,21 @@ func _physics_process(delta: float) -> void:
 	# Force the Z position and Z velocity to stay at absolute zero
 	global_position.z = 0.0
 	velocity.z = 0.0
+	
+	#State Machine
+	print("velocity.x = " + str(velocity.x))
+	if state == States.LOCKED:
+		return
+	else:
+		if velocity.x != 0 and is_on_floor() and state != States.ATTACK:
+			state = States.RUNNING
+			print("should be running")
+			
+		elif velocity.x == 0 and is_on_floor():
+			state = States.IDLE
+			
+	handle_animation()
+	
 
 func handle_horizontal_movement() -> void:
 	# Get raw horizontal input using your existing InputMap actions
@@ -38,3 +54,11 @@ func handle_jump() -> void:
 	if is_on_floor():
 		if Input.is_action_just_pressed("jump"):
 			velocity.y = jump_force
+
+func handle_animation():
+	var dictionary = {States.IDLE:"breathing_idle", States.RUNNING:"running"}
+	var dictOut = dictionary[state]
+	print(state)
+	$Base/AnimationPlayer.play(dictOut)
+	print("playing: " + dictOut)
+	return
